94abbc8bc9624a7adb87e15462404bcc7ac13c99
Macaroni0213 <macaroni.0213@gmail.com>
2020-08-09 18:12:46 +0900

    [add]eslint導入

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import Top from "./views/Top";
-import Registration from "./views/Registration";
+import Top from "./src/views/Top";
+import Registration from "./src/views/Registration";
 
 /**
  * 画面をルーティングする
