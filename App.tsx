f84531426e7b292b53c53bc86672a877488cfec5
Macaroni0213 <macaroni.0213@gmail.com>
2020-08-09 13:34:12 +0900

    [add]トップ画面、登録画面の枠だけ実装

--- App.tsx
+++ App.tsx
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Top from './views/Top';
+import Registration from './views/Registration';
+
+/**
+ * 画面をルーティングする
+ */
+const RootStack = createStackNavigator(
+  {
+    Top,
+    Registration
+  },
+  {
+    initialRouteName: 'Top',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return (<AppContainer />);
 }
 
 const styles = StyleSheet.create({
