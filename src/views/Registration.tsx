0e2d194f31acf536e5f8e79bf015790303b61cf9
Macaroni0213 <macaroni.0213@gmail.com>
2020-08-14 00:58:23 +0900

    [add]react-native-paper

--- src/views/Registration.tsx
+++ src/views/Registration.tsx
@@ -1,23 +1,65 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, SyntheticEvent } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Calendar } from "react-native-calendars";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Button } from "react-native-paper";
 
-export default class RegistrationScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>登録画面</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
-}
+const Registration = () => {
+  // 登録する買い物日付
+  const [shoppingDate, setShoppingDate] = useState(new Date());
+  // DateTimePickerを表示するかどうかのフラグ true: 表示, false: 非表示
+  const [dispDateTimePicker, setDispDateTimePicker] = useState(false);
+
+  /**
+   * DateTimePickerで選択した日付を設定する
+   * @param event
+   * @param selectedDate
+   */
+  const handleChange = (
+    event: SyntheticEvent<Readonly<{ timestamp: number }>, Event>,
+    selectedDate: Date | undefined
+  ) => {
+    const currentDate = selectedDate || shoppingDate;
+    setShoppingDate(currentDate);
+  };
+
+  /**
+   * 買い物日付入力欄にフォーカスした時にDateTimePickerを表示する
+   */
+  const handleFocus = () => {
+    setDispDateTimePicker(true);
+  };
+
+  /**
+   * 買い物日付入力欄にフォーカスした時にDateTimePickerを表示する
+   */
+  const handleBlur = () => {
+    setDispDateTimePicker(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput onFocus={handleFocus}></TextInput>
+      {dispDateTimePicker && (
+        <DateTimePicker
+          value={shoppingDate}
+          mode="date"
+          is24Hour={true}
+          onChange={handleChange}
+        />
+      )}
+      {dispDateTimePicker && <Button onPress={handleBlur}>確定</Button>}
+      <Text>登録</Text>
+    </View>
+  );
+};
+
+export default Registration;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    backgroundColor: "#fff"
+  }
 });
--- src/views/Top.tsx
+++ src/views/Top.tsx
@@ -4,26 +4,22 @@ import { StyleSheet, Text, View, Button, Dimensions } from "react-native";
 import { Badge, TouchableRipple } from "react-native-paper";
 import Ripple from "react-native-material-ripple";
 
-export default class TopScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.size = Dimensions.get("window").width / 6;
-  }
+const Top = props => {
+  const size = Dimensions.get("window").width / 6;
+  return (
+    <View style={styles.container}>
+      <Text>トップ画面</Text>
+      <StatusBar style="auto" />
+      <Ripple onPress={() => props.navigation.navigate("Registration")}>
+        <Badge size={size} visible>
+          ＋
+        </Badge>
+      </Ripple>
+    </View>
+  );
+};
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>トップ画面</Text>
-        <StatusBar style="auto" />
-        <Ripple onPress={() => this.props.navigation.navigate("Registration")}>
-          <Badge size={this.size} visible>
-            ＋
-          </Badge>
-        </Ripple>
-      </View>
-    );
-  }
-}
+export default Top;
 
 const styles = StyleSheet.create({
   container: {
